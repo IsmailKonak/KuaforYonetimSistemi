82e53d6ba6bd375f2b586650a39889a7866c0fb9
tugrayavuz258 <tugrayavuz258@gmail.com>
2024-12-23 23:44:31 +0300

    Database hata ayıklama

--- WebProjeDeneme1/WebProjeDeneme1/obj/Debug/net8.0/WebProjeDeneme1.AssemblyInfo.cs
+++ WebProjeDeneme1/WebProjeDeneme1/obj/Debug/net8.0/WebProjeDeneme1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebProjeDeneme1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31733fa4560d3ac940b01928abbbf7a4542e36c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8ad996f70a90a566d60d03531c6cd9f8ea342ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebProjeDeneme1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebProjeDeneme1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
